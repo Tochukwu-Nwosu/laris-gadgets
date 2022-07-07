95357d64818f862434e47d08a33bd1d5cd305998
unknown <nwosu4686@gmail.com>
2022-07-07 21:00:58 +0100

    modified index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-function myFunction() {
-    var obj = document.getElementById("topnav");
-    if (obj.className === "nav-links") {
-      obj.className += " responsive";
-    } else {
-      obj.className = "nav-links";
-    }
-  }
+var obj = document.getElementById("topnav");
 
-  const topNav = document.querySelector('#topnav');
-  topNav.addEventListener('click', close);
-
-  function close(){
-    var obj = document.getElementById("topnav");
-    obj.className = "nav-links";
+// This function opens and closes the .nav-link.responsive via the hamburger icon.
+function collapse() {
+  if (obj.className === "nav-links") {
+    obj.className += " responsive";     // Activates .nav-link.responsive NOTE: notice the space before 'responsive'.
+  } 
+  else {
+    obj.className = "nav-links";   // Deactivates .nav-link.responsive & Activates .nav-links
   }
+}
+
+// This function will close the .nav-links.responsive if any area of .nav-link is clicked.
+obj.addEventListener('click', close); 
+
+function close(){
+  obj.className = "nav-links";
+}
